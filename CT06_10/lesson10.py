5e93265de54e1c721cf50492dbb8019522af04a4
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-14 09:35:25 +0000

    Apr 14, 2025, 9:35 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,14 @@
 print("Hello from lesson 10")
+
+score=input("give me your score") # default, it is a string
+score=int(score) #converting this to a integer
+if score> 90:
+    print("you've gotten grade A")
+elif score >= 80:
+    print("you've gotten grade B")
+elif score >= 70:
+    print("you've gotten grade C")
+elif score >= 60:
+    print("you've gotten grade D")
+else:
+    print("you've gotten grade E")
--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,27 @@
-print("Hello from lesson 11")
+# print("Hello from lesson 11")
+# rider1 =156
+# rider2 =143
+
+# if rider1 > 120 and rider2 > 120:
+#     print("please proceed to take the ride")
+# else:
+#     print("please come back once you grow taller")
+
+
+# number=input("give me your number")
+# number=int(number)
+# if number %3==0 and %7==0
+#  print("this numberis divisible by 3 and 7!")
+
+
+
+
+
+age=input("give me your age.")
+age=int(age)
+rider1=19
+rider2=7
+if rider1 >= 18 or rider2 >= 18
+ print("you may proceed to ride.")
+ else print("you will need someone who is at least 18 to accompany you for the ride.")
+ 
