b26e7aaec9a8fa654049e7870dceff817544d287
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-14 09:51:00 +0000

    Apr 14, 2025, 9:51 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -26,9 +26,9 @@
 #  else print("you will need someone who is at least 18 to accompany you for the ride.")
  
 our=input("give me a colour: ")
-# # colour=int("colour")
-# if colour.lower() is not "green":
-#     print("try again")
+# colour=int("colour")
+if colour.lower() is not "green":
+    print("try again")
 
 
 
