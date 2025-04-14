66d794898ff847685f0497c99643650e02e9ec72
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-14 10:37:11 +0000

    Apr 14, 2025, 10:37 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,14 +1,15 @@
 print("Hello from lesson 12")
 
 word=input("give me a 5 letter word")
-(contains_e) and (contains_o) = True
+contains_e = False
+contains_o = True
 for letter in word:
     print(letter)
     if letter == "e":
          contains_e = True
     elif letter == "o":
          contains_o = True
-         
+
 if not contains_e and not contains_o:
      print("invalid word")
 else:
