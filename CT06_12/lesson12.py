6d6c5134e84507a74f1176cf5904794e3a3ab6fd
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-14 10:40:28 +0000

    Apr 14, 2025, 10:40 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,18 +1,18 @@
 print("Hello from lesson 12")
 
-word=input("give me a 5 letter word")
-contains_e = False
-contains_o = False
-for letter in word:
-    print(letter)
-    if letter == "e":
-         contains_e = True
-    if letter == "o":
-         contains_o = True
+# word=input("give me a 5 letter word")
+# contains_e = False
+# contains_o = False
+# for letter in word:
+#     print(letter)
+#     if letter == "e":
+#          contains_e = True
+#     if letter == "o":
+#          contains_o = True
 
-if not (contains_e and not contains_o):
-     print("invalid word")
-else:
-     print("good word:" + word)
+# if not (contains_e and not contains_o):
+#      print("invalid word")
+# else:
+#      print("good word:" + word)
 
            
