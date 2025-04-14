cdfe7eea3752fcc0f895867cab35ef72ec81f4c9
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-14 10:33:07 +0000

    Apr 14, 2025, 10:33 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -4,5 +4,9 @@ word=input("give me a 5 letter word")
 (contains_e) and (contains_o) = True
 for letter in word:
     print(letter)
-    
+    if letter == "e":
+         contains_e = True
+    elif letter == "o":
+         co
+
            
