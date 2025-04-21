4d6833b7385cea0edc127586a79aec2ccaec01b4
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-21 09:53:03 +0000

    Apr 21, 2025, 9:53 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,6 +1,7 @@
 order = ""
 answer = input("what is your order?")
 while not answer == "end":
+    #
     order = order + answer + ", "
     answer = input("what is your order?")
 print("you have ordered these items. do enjoy your meal!")
