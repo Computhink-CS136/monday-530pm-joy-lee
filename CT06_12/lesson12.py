44fd576c58e9bf023bd97bd0852f94cc967b69af
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-21 09:58:45 +0000

    Apr 21, 2025, 9:58 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,7 +1,10 @@
-
-    #inside the loop
+   #inside the loop
     # order = order + answer + ", "
     # answer = input("what is your order?")
 #outside the loop
 # print("you have ordered these items. do enjoy your meal!")
 # print(order)
+
+
+
+
