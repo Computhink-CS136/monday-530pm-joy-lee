d5f136aef13990a51adf41632a961647f80a9917
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-28 10:46:46 +0000

    Apr 28, 2025, 10:46 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -37,5 +37,5 @@ for item in list:
     print( item )
     if item == 'Apples':
         print( item + ": I need 5 of these")
-    if item == "Apples":
+    if item == "Carrots":
         print("I need 3 of these")
