6e357cadd4a30c23e6e3bd389cc5c57972e570c0
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-28 09:33:29 +0000

    Apr 28, 2025, 9:33 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,17 +1,20 @@
 # print("Hello from lesson 13")
-balance = 1000
-while True: 
-    print("Welcome to my bank")
-    print("choose the following:")
-    print("1. Withdraw cash")
-    print("2. Deposit cash")
-    print("3. Show balance")
-    print("4. Exit")
-    reply = input("")
-    reply = int(reply)
-    if reply == 4:
-        print("Thank you for using my bank")
-        break
-    elif reply == 1:
-        answer = input("How much to withdraw?")
+# balance = 1000
+# while True: 
+#     print("Welcome to my bank")
+#     print("choose the following:")
+#     print("1. Withdraw cash")
+#     print("2. Deposit cash")
+#     print("3. Show balance")
+#     print("4. Exit")
+#     reply = input("")
+#     reply = int(reply)
+#     if reply == 4:
+#         print("Thank you for using my bank")
+#         break
+#     elif reply == 1:
+#         answer = input("How much to withdraw?")
         
+
+
+
