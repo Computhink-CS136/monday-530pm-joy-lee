670cce1472484ef2ea77b88a68a0346a27b6875b
Computhink-CS136 <cs136@computhink.com.sg>
2025-04-28 10:02:01 +0000

    Apr 28, 2025, 10:02 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -20,7 +20,7 @@
 
 groceries = [" Apples ",  " Bread ",  " Carrots ",  " Dates ",  " Eggs ",  " Flour ",  " Grapes ",  " Honey "]
 print(groceries[7])
-groceries[5] = "Ice cream"
+groceries[8] = ""
 groceries(len(groceries) )
 print( groceries )
 
