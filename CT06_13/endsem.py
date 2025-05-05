639164a8eff1763468ee17ad19a10f9d4a55ce16
Computhink-CS136 <cs136@computhink.com.sg>
2025-05-05 10:04:06 +0000

    May 5, 2025, 10:04 AM

--- CT06_13/endsem.py
+++ CT06_13/endsem.py
@@ -51,7 +51,7 @@ planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 print (planets[2])
 
 # 2. Write code to append neptune to this list.
-
+planets
 
 # 3. Elon Musk has conquered Mars. 
 #    Rename Mars in the list to be "muskworld"
