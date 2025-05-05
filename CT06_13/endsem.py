858af744cce2072ee873f739932f54982851e39a
Computhink-CS136 <cs136@computhink.com.sg>
2025-05-05 10:19:53 +0000

    May 5, 2025, 10:19 AM

--- CT06_13/endsem.py
+++ CT06_13/endsem.py
@@ -78,6 +78,34 @@ for sphere in planets:
 
 
 
+# Write the code below each question.
+
+planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
+
+# 1. Write code below to print the 3rd item 
+#    in this list using index e.g. earth
+print (planets[2])
+
+# 2. Write code to append neptune to this list.
+planets.append("neptune")
+print (planets)
+
+# 3. Elon Musk has conquered Mars. 
+#    Rename Mars in the list to be "muskworld"
+planets[3] = "muskworld"
+print(planets)
+
+# 4. Remove uranus from this list.
+planets.pop(6)
+print(planets)
+
+# 5. Using a for loop, print all the planets 
+#    from this list one by one.
+
+for sphere in planets:
+    print (sphere)
+  
+
 
 
 
